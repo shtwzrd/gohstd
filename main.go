55b22b50b4641c972e01f79ca09bbf08c92a50ac
warreq <brandon.s.lucas@gmail.com>
2015-11-14 23:24:43 +0100

    configure PORT acquisition for Heroku

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 
 	router := NewRouter()
+	port := os.Getenv("PORT") // Get Heroku assigned PORT
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	if port == "" {
+		port = "8080" // if not on Heroku or not defined, use 8080
+	}
+
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
