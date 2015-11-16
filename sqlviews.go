19d899aa5c3f39a847c2d2597c1223033c615c2d
warreq <brandon.s.lucas@gmail.com>
2015-11-16 03:47:51 +0100

    correct the CommandHistory view

--- sqlviews.go
+++ sqlviews.go
@@ -2,11 +2,13 @@ package main
 
 const SqlCreateCommandHistoryView = `
 CREATE OR REPLACE VIEW CommandHistory AS
-SELECT DISTINCT I.invocationid,I.sessionid,I.returnstatus,I.timestamp,
+SELECT I.invocationid,I.sessionid,I.returnstatus,I.timestamp,
                 CT.hostname,CT.username,CT.shell,CT.directory,CM.commandstring,
                 ARRAY(SELECT TA.name
                       FROM(invocationtag TI LEFT OUTER JOIN tag T
                            ON (TI.tagid = T.tagid)) TA
                       WHERE TA.invocationid = I.invocationid) AS tags
-FROM invocation I, "session" S, context CT, command CM;
+FROM invocation I INNER JOIN "session" S ON (I.sessionid = S.sessionid)
+     INNER JOIN context CT ON (S.contextid = CT.contextid)
+     INNER JOIN command CM ON (I.commandid = CM.commandid);
 `
