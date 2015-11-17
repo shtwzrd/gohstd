e7b64e8bb9310d582f2e6b8d187e0ad06a9859f9
warreq <brandon.s.lucas@gmail.com>
2015-11-17 17:23:44 +0100

    include drop trigger statement when starting up

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -1,6 +1,7 @@
 -- everytime insert in invocation check how many invocation use that command id
 -- if >20 then create a record in notification table
 
+DROP TRIGGER IF EXISTS command_notification ON invocation
 CREATE TRIGGER command_notification
-AFTER INSERT ON invocation 
+AFTER INSERT ON invocation
 FOR EACH ROW EXECUTE PROCEDURE command_notification_insert();
