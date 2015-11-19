07400ab0bf1d7e7db57c40af98fee1708b77d147
warreq <brandon.s.lucas@gmail.com>
2015-11-19 03:40:42 +0100

    change size limit

--- handlers.go
+++ handlers.go
@@ -61,7 +61,7 @@ func CommandCreate(w http.ResponseWriter, r *http.Request) {
 	user := vars["username"]
 
 	var inv Invocations
-	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576000))
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 10485760))
 	if err != nil {
 		w.Header().Set("Content-Type", "plaintext; charset=UTF-8")
 		w.WriteHeader(http.StatusBadRequest)
