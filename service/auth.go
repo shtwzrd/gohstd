c87cddd0b648a0bda45cfe853920845b976794b6
warreq <brandon.s.lucas@gmail.com>
2015-12-27 23:36:30 +0100

    implement authorization based on ownership
    Note:
    AuthorizationFilters are currently only set up such that all must
    pass in order proceed. This might not be expressive enough in the
    future.

--- service/auth.go
+++ service/auth.go
@@ -2,28 +2,43 @@ package service
 
 import (
 	"errors"
+	"github.com/gorilla/mux"
 	g "github.com/warreq/gohstd/common"
 	bcrypt "golang.org/x/crypto/bcrypt"
 	"log"
 	"net/http"
 )
 
-// AuthenticationBlackList contains the sets of serivce/Routes for which
+// AuthorizationFilter is a function type used for determining if a user is
+// authorized to perform a given request. It returns true if a user meets the
+// requirements of the authorization logic, and false otherwise.
+type AuthorizationFilter func(user string, r *http.Request) bool
+
+// AuthenticationBlackList contains the sets of service/Routes for which
 // authentication is not necessary. Routes listed here bypass the authentication
 // middleware and subsequently the authorization middleware as well, as to allow
 // anonymous access to those endpoints.
 var AuthenticationBlackList map[string]struct{}
 
-// AuthorizationBlackList contains the sets of serivce/Routes for which
-// authorization is not necessary, but authentication is required.
-var AuthorizationBlackList map[string]struct{}
+// AuthorizationFilters contains the slice of functions for each service/Route
+// to use for authorizing that endpoint. If a Route isn't found in the map, it
+// requires no authorization
+var AuthorizationFilters map[string][]AuthorizationFilter
 
 func init() {
 	AuthenticationBlackList = map[string]struct{}{
 		"UserRegister": {},
 	}
 
-	AuthorizationBlackList = map[string]struct{}{}
+	AuthorizationFilters = map[string][]AuthorizationFilter{
+		"CommandIndex":     []AuthorizationFilter{RequesterOwnsResource},
+		"CommandCreate":    []AuthorizationFilter{RequesterOwnsResource},
+		"CommandDelete":    []AuthorizationFilter{RequesterOwnsResource},
+		"CommandTagCreate": []AuthorizationFilter{RequesterOwnsResource},
+		"UserTagShow":      []AuthorizationFilter{RequesterOwnsResource},
+		"UserTagRename":    []AuthorizationFilter{RequesterOwnsResource},
+		"UserTagDelete":    []AuthorizationFilter{RequesterOwnsResource},
+	}
 }
 
 // Auth is the http.Handler middleware for performinag authentication and
@@ -44,7 +59,7 @@ func Auth(inner http.Handler, route string) http.Handler {
 			HttpError(w, http.StatusBadRequest, errors.New(g.InvalidBasicAuthError))
 			return
 		}
-		authenticated := Authenticate(username, password, route)
+		authenticated := Authenticate(username, password)
 		if !authenticated {
 			HttpError(w, http.StatusUnauthorized, nil)
 			return
@@ -59,7 +74,11 @@ func Auth(inner http.Handler, route string) http.Handler {
 	})
 }
 
-func Authenticate(user, password, route string) bool {
+// Authenticate compares the user and password provided and compares the stored
+// credential for the given user against the given password. Authentication
+// returns true in the case that the given password matches the stored
+// credential and false otherwise.
+func Authenticate(user, password string) bool {
 	identity, err := userRepo.GetUserByName(user)
 	if err != nil {
 		log.Println(err)
@@ -74,6 +93,22 @@ func Authenticate(user, password, route string) bool {
 	}
 }
 
+// Authorize searches the AuthorizationFilters for functions pertaining to the
+// given request and applies each filter in order. If any filter returns false,
+// Authorize returns false. Otherwise and including the case of no filters,
+// Authorize returns true.
 func Authorize(user string, r *http.Request, route string) bool {
+	for _, filter := range AuthorizationFilters[route] {
+		if !filter(user, r) {
+			return false
+		}
+	}
 	return true
+}
+
+func RequesterOwnsResource(user string, r *http.Request) bool {
+	vars := mux.Vars(r)
+	owner := vars["username"]
+
+	return user == owner
 }
