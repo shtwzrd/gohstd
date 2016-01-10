87e1f08a00656319b1ac832c81d42f5593b28a8f
TheMightyMarm <clovis.lebret@gmail.com>
2016-01-10 14:06:34 +0100

    pic preview, and FUCK YOU TYPE ERRORS

--- webapp/app/configuration.component.ts
+++ webapp/app/configuration.component.ts
@@ -1,4 +1,4 @@
-import {Component} from 'angular2/core';
+import {Component,OnChanges, SimpleChange,} from 'angular2/core';
 import {NgForm} from 'angular2/common';
 import {Configuration, NewConfiguration} from './configuration';
 import {FILE_UPLOAD_DIRECTIVES, FileUploader} from 'ng2-file-upload';
@@ -12,7 +12,22 @@ export class ConfigurationComponent {
 	model = new NewConfiguration("");
 	
 	submitConfiguration(){
-		alert(this.model.picture);
+		console.log(this.model);
 	}
+	
+	picturePreview(){
+		var preview = document.querySelector('img');
+		var file    = (<HTMLInputElement>document.querySelector('input[type=file]')).files[0];
+		var reader  = new FileReader();
 
+		reader.onloadend = function () {
+			(<HTMLImageElement>preview).src = reader.result;
+		}
+
+		if (file) {
+			reader.readAsDataURL(file);
+		} else {
+			(<HTMLImageElement>preview).src = "";
+		}
+	}
 }
